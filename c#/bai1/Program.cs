38fe61eb4cd294efa405e91ac98bf95f427da08d
HuyDwin19 <vuhuy19032006@gmail.com>
2025-10-24 21:28:02 +0700

    buoi2

---
+++ c#/bai1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace bai1
+{  class Program{
+    static void Main (string[] args){
+    Console.Write("nhap cac canh ");
+    double a = double.Parse(Console.ReadLine());
+    double b = double.Parse(Console.ReadLine());
+    double c = double.Parse(Console.ReadLine());
+            if (a + b < c)
+            {
+                Console.WriteLine("is valid triangle");
+            }
+            else if (a == b || b == c || c == a)
+            {
+                Console.WriteLine("is a isosceles triangle");
+            }
+            else if (a == b && b == c)
+            {
+                Console.WriteLine("is a equilateral triangle");
+            }
+            else
+            {
+                Console.WriteLine("is a nomal triangle");
+            }
+}}}
---
+++ c#/bai3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace bai3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = n -i-1; j > 0; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i + 1; k++)
+                {
+                    Console.Write(k);
+                }
+                for (int  l = i ; l >=1 ; l--)
+                {
+                    Console.Write(l);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
