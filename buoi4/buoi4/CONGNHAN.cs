e887d1184f601acdc7547b5fc8d07764f514af82
vu quang huy <vuhuy19032006@gmail.com>
2025-11-08 00:11:23 +0700

    buoi4

---
+++ buoi4/buoi4/CONGNHAN.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buoi4
+{     public enum ChucVu
+        {
+            TruongNhom,
+            PhoNhom,
+            CongNhanBac3,
+            CongNhanBac2,
+            CongNhanBac1,
+            Khac
+        }
+    internal class CONGNHAN : Person
+    {   public string maCongNhan { get; set; }
+        public ChucVu chucVu { get; set; }
+        public CONGNHAN(string macongnhan,ChucVu chucvu,string name, int age, string address): base(name,age,address){
+               maCongNhan= macongnhan;
+               chucVu= chucvu;
+         }
+        private double x = 8000000;
+        public CONGNHAN() { }
+        public double TinhLuong()
+        {
+            switch (chucVu)
+            {
+                case ChucVu.TruongNhom:
+                    return 3.0 * x;
+                case ChucVu.PhoNhom:
+                    return 2.5 * x;
+                case ChucVu.CongNhanBac3 :
+                    return 2 * x;
+                case ChucVu.CongNhanBac2:
+                    return 1.5 * x;
+                case ChucVu.CongNhanBac1 :
+                    return 1.2 * x;
+                default:
+                    return 1 * x;
+            }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + " id: " + maCongNhan+ " Chuc vu: " + chucVu;
+        }
+    }
+}
---
+++ buoi4/buoi4/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+namespace buoi4
+{
+    class Program
+    { static private List<CONGNHAN> DSCN = new List<CONGNHAN>();
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("-------MENU----------");
+                Console.WriteLine("=====================");
+                Console.WriteLine("1.Them cong nhan ");
+                Console.WriteLine("2.Hien thi danh sach cong nhan");
+                Console.WriteLine("3.Sap xep danh sach cong nhan");
+                Console.WriteLine("4.Tim cong nhan theo ma");
+                Console.Write("nhap lua chon: ");
+                switch (Console.ReadLine())
+                {
+                    case "1": Add(); break;
+                    case "2": Output(); break;
+                    case "3": Sort(); break;
+                    case "4": Finding(); break;
+                    default:
+                        break;
+                }
+            } while (true);
+        }
+        static private void Add()
+        {
+            while (true) {
+                Console.Write("Nhap ma cong nhan "); string id = Console.ReadLine();
+                Console.Write("Nhap chuc vu: \n 1. Truong nhom \n 2. Pho nhom \n 3. Cong nhan bac 3 \n 4. Cong nhan bac 2 \n 5. Cong nhan bac 1 \n");
+                ChucVu cv = (ChucVu)(int.Parse(Console.ReadLine())-1);
+                foreach (var item in DSCN) {
+                    if (item.maCongNhan.Equals(id))
+                    {
+                        Console.WriteLine("is exist !");
+                        break;
+                    }
+                }
+                Console.Write("Nhap ten "); string name = Console.ReadLine();
+                int age;
+                do
+                {
+                    Console.Write("nhap tuoi "); age = int.Parse(Console.ReadLine());
+                } while (age < 0);
+                Console.Write("Nhap address "); string address = Console.ReadLine();
+                CONGNHAN x = new CONGNHAN(id, cv, name, age, address);
+                DSCN.Add(x);
+                Console.WriteLine("Nhap tiep : Y/N");
+                string option = Console.ReadLine();
+                if (option.Equals("N"))
+                {
+                    break;
+                }
+            }
+        }
+        static private void Output()
+        {
+            foreach (var item in DSCN)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+        static private void Sort()
+        {
+            DSCN.Sort((a, b) =>
+            {
+                int kq = a.Name.CompareTo(b.Name);
+                if (kq == 0)
+                {
+                    return a.TinhLuong().CompareTo(b.TinhLuong());
+                }
+                return kq;
+            });
+            Output();
+        }
+
+        static private void Finding()
+        { Console.Write("Nhap ma can tim "); string id = Console.ReadLine();
+            foreach (var item in DSCN)
+            {
+                if (item.maCongNhan.Equals(id))
+                {
+                    Console.WriteLine(item.ToString());
+                }
+            }
+
+        }
+     
+    }
+}
+
